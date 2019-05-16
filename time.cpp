6b4067e5f97830e3eef852794e36ab7f467a1645
ArturMalec <50462677+ArturMalec@users.noreply.github.com>
2019-05-16 17:54:54 +0200

    Add files via upload

---
+++ time.cpp
@@ -0,0 +1,58 @@
+/*
+ * time.cpp
+ *
+ *  Created on: 15 paü 2018
+ *      Author: Malcini
+ */
+
+#include <iostream>
+#include <windows.h>
+float time(float, float);
+
+int main()
+{
+	float track;
+	float speed;
+	float func;
+	int hours, minutes;
+	char choice;
+
+	std::cout << "|| Program caluculates time needed to traveling the route ||\n\n";
+	do
+	{
+	std::cout << "Enter distance and average speed: ";
+	while (std::cin >> track >> speed)
+	{
+		try
+		{
+			func = time(track, speed);
+			hours = func;
+			minutes = (func - hours) * 60;
+		}
+		catch (const char * s)
+		{
+			std::cout << s << std::endl;
+			std::cout << "Enter data ones again: ";
+			continue;
+	}
+	std::cout << "Time for travel: " << hours << " h. " << minutes << " min." << std::endl;
+	break;
+	}
+	Sleep(1000);
+	std::cout << "Ones Again? T/N >> ";
+	std::cin >> choice;
+	}
+	while (choice == 84 || choice == 116);
+	std::cout << "Closing program . . ." << std::endl;
+	Sleep(3000);
+	return 0;
+}
+
+float time(float a, float b)
+{
+	if (a < 0 || b < 0)
+		throw "Digit mustn't be lower than zero!\n";
+	float time_;
+	time_ = a/b;
+	return time_;
+}
